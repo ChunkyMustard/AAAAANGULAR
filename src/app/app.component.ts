cc05162f6488de7bef2ce1a3f8730799a870d5a1
Peter Green <fishlegs10@gmail.com>
2020-07-06 22:06:21 -0400

    ANGULAR MORE LIKE AAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-lab';
+
+  todos :Todo[]= [
+    {
+        task: "run",
+        completed: false
+    },
+    {
+        task: "walk",
+        completed: false
+    },
+    {
+        task: "crawl",
+        completed: false
+    },
+    {
+        task: "cry",
+        completed: true
+    },
+    {
+        task: "fly",
+        completed: false
+    },
+];
 }
+
+interface Todo
+{
+    task: string;
+    completed: boolean;
+}
+
+
+
+
+// ng serve
